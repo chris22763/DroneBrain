c19d8bd3bcb800394a7809c6b2e06f42d94bc3b0
chris22763 <cmuehrke@me.com>
2018-10-04 21:21:56 +0200

    close

--- remote_calib/piCapture.py
+++ remote_calib/piCapture.py
@@ -192,7 +192,7 @@ def captureSerialImage(camera,left,frameId, imageSize):
             ret, corners = cv2.findCirclesGrid(im_with_keypoints, pattern_size, None, flags = cv2.CALIB_CB_ASYMMETRIC_GRID)   # Find the circle grid
 
             # clear the stream in preparation for the next frame
-            camera.truncate()
+            camera.close()
 
             return ret, corners, im_with_keypoints, im_with_keypoints_gray
 

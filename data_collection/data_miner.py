2f6c952073d4035b5c2278d3632ae57760d73f55
chris22763 <cmuehrke@me.com>
2019-01-02 20:35:09 +0100

    Update data_miner.py

--- data_collection/data_miner.py
+++ data_collection/data_miner.py
@@ -24,8 +24,7 @@ def edit_sys_path():
                    '/home/up/.virtualenvs/drone/local/lib/python3.5/site-packages',
                    '../Adafruit_Python_GPIO',
                    '../Adafruit_Python_BNO055']
-    path_to_delete = ['/usr/local/lib/python3.5/dist-packages/Adafruit_GPIO-1.0.0-py3.5.egg',
-                      '/home/up/.virtualenvs/drone/lib/python3.5/site-packages']
+    path_to_delete = ['/usr/local/lib/python3.5/dist-packages/Adafruit_GPIO-1.0.0-py3.5.egg']
     for to_del in path_to_delete:
         if to_del in sys.path:
             sys.path.remove(to_del)

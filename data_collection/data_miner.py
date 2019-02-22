e45ebd6eef4c8f7427949a9492327c28e7797650
chris22763 <cmuehrke@me.com>
2019-02-22 19:34:59 +0100

    Update data_miner.py

--- data_collection/data_miner.py
+++ data_collection/data_miner.py
@@ -156,8 +156,8 @@ def append_to_img(img, data):
         for i in range(data_len, img_width):
             data.append([0, 0, 0])
         npdata = np.array(data)
-        print(npdata.size[:2])
-        print(img.size[:2])
+        print(npdata.__len__())
+        print(img.__len__())
 
         out = np.concatenate(img , npdata)
 

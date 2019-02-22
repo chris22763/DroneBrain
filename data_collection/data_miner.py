025a7edf0370c0b40ede16e299b64c07813cce84
chris22763 <cmuehrke@me.com>
2019-02-22 22:27:31 +0100

    Update data_miner.py

--- data_collection/data_miner.py
+++ data_collection/data_miner.py
@@ -278,7 +278,8 @@ try:
         cv2.imshow('Data_IMG', data_image)
 
         big = cv2.resize(data_row, (0,0), fx=10, fy=10)
-        cv2.imshow('Big_Data_IMG', big)
+        big_img = np.asanyarray(big)
+        cv2.imshow('Big_Data_IMG', big_img)
         cv2.waitKey(1)
 
 

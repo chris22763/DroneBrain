e563fcdd4c46427d47d56847cc3340421442ed37
chris22763 <cmuehrke@me.com>
2019-10-18 10:23:03 +0200

    Update thalamus.py

--- the_real_skynet/thalamus.py
+++ the_real_skynet/thalamus.py
@@ -151,14 +151,23 @@ class Thalamus:
 
 
     def init_gps(self):
-        from sys import argv
-        import gps
         import requests
+        from gps3 import gps3
+        gps_socket = gps3.GPSDSocket()
+        data_stream = gps3.DataStream()
+        gps_socket.connect()
+        gps_socket.watch()
+        for new_data in gps_socket:
+            if new_data:
+                data_stream.unpack(new_data)
+
+                print('Altitude = ', data_stream.TPV['alt'])
+                print('Latitude = ', data_stream.TPV['lat'])
 
         #Listen on port 2947 of gpsd
-        session = gps.gps("localhost", "2947")
-        session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
-        return session
+        #session = gps.gps("localhost", "2947")
+        #session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+        return data_stream
 
 
     def init_wifi(self):

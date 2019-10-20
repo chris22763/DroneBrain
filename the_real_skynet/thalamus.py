1f041ed3d6f50caf2b4a25e98b7b8a833e0f3184
chris22763 <cmuehrke@me.com>
2019-10-20 20:40:54 +0200

    Update thalamus.py

--- the_real_skynet/thalamus.py
+++ the_real_skynet/thalamus.py
@@ -167,23 +167,6 @@ class Thalamus:
         #         print('Latitude = ', data_stream.TPV['lat'])
         # Listen on port 2947 of gpsd
 
-        while True:
-            try:
-                report = session.next()
-                # Wait for a 'TPV' report and display the current time
-                # To see all report data, uncomment the line below
-                # print(report)
-                if report['class'] == 'TPV':
-                    if hasattr(report, 'time'):
-                        print(report.time)
-            except KeyError:
-                pass
-            except KeyboardInterrupt:
-                quit()
-            except StopIteration:
-                session = None
-                print("GPSD has terminated")
-
         session = gps.gps("localhost", "2947")
         session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
         self.get_gps(session)
@@ -243,6 +226,7 @@ class Thalamus:
         try :
             if rep["class"] == "TPV":
                 print(str(rep.lat) + "," + str(rep.lon))
+                self.sensor_data['GPS'] = [rep.lat, rep.lon]
                 return [rep.lat, rep.lon]
 
         except Exception as e :

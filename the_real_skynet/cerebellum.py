f4c748a9ec0f45f55213f595061159d097626a21
chris22763 <cmuehrke@me.com>
2019-10-25 13:42:52 +0200

    Update cerebellum.py

--- the_real_skynet/cerebellum.py
+++ the_real_skynet/cerebellum.py
@@ -90,7 +90,7 @@ class Cerebellum ():
         for i, p in enumerate(blossom):
             color = RED if p not in pset else GREEN
 
-            cv2.circle(img_rgb, (p[1],p[0]), 3, (255, 255, 255), -1)
+            cv2.circle(img_rgb, (p[1],p[0]), 3, color, -1)
 
         cv2.namedWindow('targets',cv2.WINDOW_AUTOSIZE)
         cv2.imshow('targets', img_rgb)

0e159223232d4d9a372b13be73c368bc62fbaaac
chris22763 <cmuehrke@me.com>
2019-11-13 19:56:06 +0100

    Update cerebellum.py

--- the_real_skynet/cerebellum.py
+++ the_real_skynet/cerebellum.py
@@ -163,9 +163,10 @@ class Cerebellum ():
 
     def distance_in_pixel(self, val):
         max_val = 65536  # 65536 is max value
+        d_unit = 1000 # depth unit
         val = val if val != max_val else max_val-1
         # dim = ((1/(val - max_val))*-10)-1   # if val 0..255  # -1 to make 1..10m to 0..9m
-        dim = (val * (1000/max_val)/100)
+        dim = (val/d_unit)
         # dim = val * 10           # if val 0.0 .. 1.0
         # _d = (int(260/dim), int(120/dim))  # 130x60@2m and 1m x 0.5m 
         _d = (int(130/dim), int(60/dim))  # only half the pixel ammount is needed. 130 => 1m

3b975b87e6e64818238f37beef435446559a32af
chris22763 <cmuehrke@me.com>
2019-11-14 15:18:30 +0100

    Update cerebellum.py

--- the_real_skynet/cerebellum.py
+++ the_real_skynet/cerebellum.py
@@ -190,8 +190,8 @@ class Cerebellum ():
 
 
     def check_flower(self, img):
-        obst = np.array([[],], np.int16)  # Obstacle
-        free = np.array([[],], np.int16)
+        obst = np.zeros((1,2), np.int16)  # Obstacle
+        free = np.zeros(1,2), np.int16)
         # start = time.time()
 
         print(self.flower)

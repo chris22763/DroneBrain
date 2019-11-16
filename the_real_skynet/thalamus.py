356fcc27aadf803d3b498a254b71c96235f43953
chris22763 <cmuehrke@me.com>
2019-11-16 13:43:57 +0100

    Update thalamus.py

--- the_real_skynet/thalamus.py
+++ the_real_skynet/thalamus.py
@@ -164,7 +164,7 @@ class Thalamus:
             # max_x = 424
             # max_y = 240
 
-            self.resolution = (max_x, max_y)
+            self.resolution = (720, 1280)
 
             # todo custom config
             pipeline = rs.pipeline()

7a8360c08891a4925057ea51ca3a1dc3cca5d544
chris22763 <cmuehrke@me.com>
2019-11-27 00:04:13 +0100

    Update cerebellum.py

--- the_real_skynet/cerebellum.py
+++ the_real_skynet/cerebellum.py
@@ -238,7 +238,8 @@ class Cerebellum ():
         threadsperblock = 16
         blockspergrid =  ((free.__len__() + (threadsperblock)) // threadsperblock)-1
         print('cuda block setting [{}, {}]'.format(blockspergrid, threadsperblock))
-        nucleusfastigii.check_corridor_kernel[blockspergrid, threadsperblock](free, obst, potantial_target, depth_np)
+        nucleusfastigii.check_corridor_kernel[blockspergrid, threadsperblock](free, obst, potantial_target, depth_np,
+                                                                              len(obst), len(potantial_target)
 
         """
         square = set()
